30bcd59c8103c9faddbbf0a13ecfee850c285df5
Valodax <snitzel098@gmail.com>
2022-03-30 19:07:27 +0200

    deletions

--- arbitrage.py
+++ arbitrage.py
@@ -7,7 +7,6 @@ import pandas as pd
 request = cg.get_exchanges_id_name_list()
 data1 = request
 df1 = pd.DataFrame(data1)
-#print(df1['id'][77]) #gets exchange id in the id column of the exchange list panda table
 exchanges_df = pd.DataFrame()
 for exchange_name in df1['id']:
     print(exchange_name)
@@ -20,7 +19,4 @@ for exchange_name in df1['id']:
         exchanges_df = exchanges_df.append(df2)
 
 print('end')
-# Query Exchanges for Coins
-#request2 = cg.get_exchanges_tickers_by_id(id=exchange_name,coin_ids='chainlink')
-#data2 = request2.get('tickers')
-#df2 = pd.DataFrame(data2)
+
