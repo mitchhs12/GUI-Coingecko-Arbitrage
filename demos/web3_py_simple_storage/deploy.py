55c252dafd953103f36c1beba6e939cc0cdee275
Valodax <48880504+Valodax@users.noreply.github.com>
2022-05-13 00:38:31 +0200

    lesson 4 complete

--- demos/web3_py_simple_storage/deploy.py
+++ demos/web3_py_simple_storage/deploy.py
@@ -38,51 +38,60 @@ bytecode = compiled_sol["contracts"]["SimpleStorage.sol"]["SimpleStorage"]["evm"
 ]["object"]
 
 # get abi
-abi = compiled_sol["contracts"]["SimpleStorage.sol"]["SimpleStorage"]["abi"]
+abi = json.loads(
+    compiled_sol["contracts"]["SimpleStorage.sol"]["SimpleStorage"]["metadata"]
+)["output"]["abi"]
 
-# for connecting to ganache
-w3 = Web3(Web3.HTTPProvider("HTTP://127.0.0.1:8545"))
+# w3 = Web3(Web3.HTTPProvider(os.getenv("RINKEBY_RPC_URL")))
+# chain_id = 4
+#
+# For connecting to ganache
+w3 = Web3(Web3.HTTPProvider("http://127.0.0.1:8545"))
 chain_id = 1337
-my_address = "0x9e98aCa5b01B73d299a6a4A2C2fF6881823215a3"
+my_address = "0x8fbebeE61fdD723Bf3C0c01E3074c1dB4F0A0a6E"
 private_key = os.getenv("PRIVATE_KEY")
-print(private_key)
 
-# Create the contract in python
+# Create the contract in Python
 SimpleStorage = w3.eth.contract(abi=abi, bytecode=bytecode)
-
 # Get the latest transaction
 nonce = w3.eth.getTransactionCount(my_address)
-
+# Submit the transaction that deploys the contract
 transaction = SimpleStorage.constructor().buildTransaction(
-    {"chainId": chain_id, "from": my_address, "nonce": nonce}
+    {
+        "chainId": chain_id,
+        "gasPrice": w3.eth.gas_price,
+        "from": my_address,
+        "nonce": nonce,
+    }
 )
-
+# Sign the transaction
 signed_txn = w3.eth.account.sign_transaction(transaction, private_key=private_key)
-
-# Send this signed transaction
-print("Deploying contract...")
+print("Deploying Contract!")
+# Send it!
 tx_hash = w3.eth.send_raw_transaction(signed_txn.rawTransaction)
+# Wait for the transaction to be mined, and get the transaction receipt
+print("Waiting for transaction to finish...")
 tx_receipt = w3.eth.wait_for_transaction_receipt(tx_hash)
-print("Deployed!")
+print(f"Done! Contract deployed to {tx_receipt.contractAddress}")
 
-# Working with the contract
-# Contract Address
-# Contract abi
-
+# Working with deployed Contracts
 simple_storage = w3.eth.contract(address=tx_receipt.contractAddress, abi=abi)
-# call -> simulate making the call and getting a return value
-# transact -> actually make a state change
+print(f"Initial Stored Value {simple_storage.functions.retrieve().call()}")
+greeting_transaction = simple_storage.functions.store(15).buildTransaction(
+    {
+        "chainId": chain_id,
+        "gasPrice": w3.eth.gas_price,
+        "from": my_address,
+        "nonce": nonce + 1,
+    }
+)
+signed_greeting_txn = w3.eth.account.sign_transaction(
+    greeting_transaction, private_key=private_key
+)
 
-# Initial value of favourite number
-print(simple_storage.functions.retrieve().call())
-print("Updating contract...")
-store_transaction = simple_storage.functions.store(15).buildTransaction(
-    {"chainId": chain_id, "from": my_address, "nonce": nonce + 1}
-)
-signed_store_txn = w3.eth.account.sign_transaction(
-    store_transaction, private_key=private_key
-)
-send_store_tx = w3.eth.send_raw_transaction(signed_store_txn.rawTransaction)
-tx_receipt = w3.eth.wait_for_transaction_receipt(send_store_tx)
+tx_greeting_hash = w3.eth.send_raw_transaction(signed_greeting_txn.rawTransaction)
+print("Updating stored Value...")
+tx_receipt = w3.eth.wait_for_transaction_receipt(tx_greeting_hash)
 print("Updated!")
+
 print(simple_storage.functions.retrieve().call())
