ef04735665c7983338d32855745ce1fe0897760c
Valodax <48880504+Valodax@users.noreply.github.com>
2022-05-12 23:25:56 +0200

    lesson 4

--- demos/web3_py_simple_storage/SimpleStorage.sol
+++ demos/web3_py_simple_storage/SimpleStorage.sol
@@ -15,8 +15,9 @@ contract SimpleStorage {
     People[] public people;
     mapping(string => uint256) public nameToFavoriteNumber;
 
-    function store(uint256 _favoriteNumber) public {
+    function store(uint256 _favoriteNumber) public returns (uint256) {
         favoriteNumber = _favoriteNumber;
+        return _favoriteNumber;
     }
 
     function retrieve() public view returns (uint256) {
--- demos/web3_py_simple_storage/deploy.py
+++ demos/web3_py_simple_storage/deploy.py
@@ -1,3 +1,88 @@
+from solcx import compile_standard, install_solc
+import json
+from web3 import Web3
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
 with open("./SimpleStorage.sol", "r") as file:
     simple_storage_file = file.read()
-    print(simple_storage_file)
+
+# Compile Our Solidity
+print("Installing...")
+install_solc("0.6.0")
+
+# Solidity source code
+compiled_sol = compile_standard(
+    {
+        "language": "Solidity",
+        "sources": {"SimpleStorage.sol": {"content": simple_storage_file}},
+        "settings": {
+            "outputSelection": {
+                "*": {
+                    "*": ["abi", "metadata", "evm.bytecode", "evm.bytecode.sourceMap"]
+                }
+            }
+        },
+    },
+    solc_version="0.6.0",
+)
+
+with open("compiled_code.json", "w") as file:
+    json.dump(compiled_sol, file)
+
+# get bytecode
+bytecode = compiled_sol["contracts"]["SimpleStorage.sol"]["SimpleStorage"]["evm"][
+    "bytecode"
+]["object"]
+
+# get abi
+abi = compiled_sol["contracts"]["SimpleStorage.sol"]["SimpleStorage"]["abi"]
+
+# for connecting to ganache
+w3 = Web3(Web3.HTTPProvider("HTTP://127.0.0.1:8545"))
+chain_id = 1337
+my_address = "0x9e98aCa5b01B73d299a6a4A2C2fF6881823215a3"
+private_key = os.getenv("PRIVATE_KEY")
+print(private_key)
+
+# Create the contract in python
+SimpleStorage = w3.eth.contract(abi=abi, bytecode=bytecode)
+
+# Get the latest transaction
+nonce = w3.eth.getTransactionCount(my_address)
+
+transaction = SimpleStorage.constructor().buildTransaction(
+    {"chainId": chain_id, "from": my_address, "nonce": nonce}
+)
+
+signed_txn = w3.eth.account.sign_transaction(transaction, private_key=private_key)
+
+# Send this signed transaction
+print("Deploying contract...")
+tx_hash = w3.eth.send_raw_transaction(signed_txn.rawTransaction)
+tx_receipt = w3.eth.wait_for_transaction_receipt(tx_hash)
+print("Deployed!")
+
+# Working with the contract
+# Contract Address
+# Contract abi
+
+simple_storage = w3.eth.contract(address=tx_receipt.contractAddress, abi=abi)
+# call -> simulate making the call and getting a return value
+# transact -> actually make a state change
+
+# Initial value of favourite number
+print(simple_storage.functions.retrieve().call())
+print("Updating contract...")
+store_transaction = simple_storage.functions.store(15).buildTransaction(
+    {"chainId": chain_id, "from": my_address, "nonce": nonce + 1}
+)
+signed_store_txn = w3.eth.account.sign_transaction(
+    store_transaction, private_key=private_key
+)
+send_store_tx = w3.eth.send_raw_transaction(signed_store_txn.rawTransaction)
+tx_receipt = w3.eth.wait_for_transaction_receipt(send_store_tx)
+print("Updated!")
+print(simple_storage.functions.retrieve().call())
