07de1c6e7681720e404a6ee2f4e56a2f0a8c6ad2
MSpencer <48880504+Valodax@users.noreply.github.com>
2022-05-14 19:01:30 +0200

    Delete demos directory

--- demos/brownie_fund_me/scripts/helpful_scripts.py
+++
@@ -1,8 +0,0 @@
-from brownie import network, config, accounts
-
-
-def get_account():
-    if network.show_active() == "development":
-        return accounts[0]
-    else:
-        return accounts.add(config["wallets"]["from_key"])
